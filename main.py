21d210be6519d0fcd53b6a6354a53eba54f58d00
Michelle <36121618+mdezetter@users.noreply.github.com>
2023-09-21 21:50:24 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,2 @@
+basic.show_string("I'm Michelle!")
+basic.show_string("I like Chicago, Data, and Math")
---
+++ main.ts
@@ -0,0 +1,2 @@
+basic.showString("I'm Michelle!")
+basic.showString("I like Chicago, Data, and Math")
